7b124d5a2165593621896ccd450089f31a7a5eb3
Marvin Buss <marvin.buss@gmail.com>
2024-11-21 17:53:57 +0100

    Update platform output ids

--- modules/platform/outputs.tf
+++ modules/platform/outputs.tf
@@ -1,28 +1,28 @@
-output "subnet_storage_id" {
+output "subnet_id_storage" {
   description = "Specifies the storage subnet id."
   sensitive = false
   value = "${azapi_update_resource.virtual_network.id}/subnets/${local.subnet_storage.name}"
 }
 
-output "subnet_fabric_id" {
+output "subnet_id_fabric" {
   description = "Specifies the fabric subnet id."
   sensitive = false
   value = "${azapi_update_resource.virtual_network.id}/subnets/${local.subnet_fabric.name}"
 }
 
-output "subnet_consumption_private_id" {
+output "subnet_id_consumption_private" {
   description = "Specifies the private consumption subnet id."
   sensitive = false
   value = "${azapi_update_resource.virtual_network.id}/subnets/${local.subnet_consumption_private.name}"
 }
 
-output "subnet_consumption_public_id" {
+output "subnet_id_consumption_public" {
   description = "Specifies the public consumption subnet id."
   sensitive = false
   value = "${azapi_update_resource.virtual_network.id}/subnets/${local.subnet_consumption_public.name}"
 }
 
-output "subnets_private_endpoint_application_ids" {
+output "subnet_ids_private_endpoint_application" {
   description = "Specifies the application private endpoint subnet ids."
   sensitive = false
   value = {
@@ -31,7 +31,7 @@ output "subnets_private_endpoint_application_ids" {
   }
 }
 
-output "subnets_databricks_private_application_ids" {
+output "subnet_ids_databricks_private_application" {
   description = "Specifies the application databricks private subnet ids."
   sensitive = false
   value = {
@@ -40,7 +40,7 @@ output "subnets_databricks_private_application_ids" {
   }
 }
 
-output "subnets_databricks_public_application_ids" {
+output "subnet_ids_databricks_public_application" {
   description = "Specifies the application databricks public subnet ids."
   sensitive = false
   value = {
