186b7a21d54419d4e63f919a604b1e49b6a2fdc9
Marvin Buss <marvin.buss@gmail.com>
2024-11-29 20:10:31 +0100

    Add account provider config

--- providers.tf
+++ providers.tf
@@ -9,3 +9,13 @@ provider "databricks" {
   rate_limit                  = 15
   skip_verify                 = false
 }
+
+provider "databricks" {
+  alias                = "account"
+  azure_environment    = "public"
+  host                 = "https://accounts.azuredatabricks.net"
+  account_id           = var.databricks_account_id
+  http_timeout_seconds = 60
+  rate_limit           = 15
+  skip_verify          = false
+}
