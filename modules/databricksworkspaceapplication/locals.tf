e1e038699d2e3bd2b791affdd880314ebc6c0bef
Marvin Buss <marvin.buss@gmail.com>
2024-12-13 00:26:52 +0100

    Update value

--- modules/databricksworkspaceapplication/locals.tf
+++ modules/databricksworkspaceapplication/locals.tf
@@ -31,6 +31,6 @@ locals {
   )
   databricks_cluster_policies = {
     for key, value in var.databricks_cluster_policies :
-    key => templatestring(jsondecode(jsonencode(value)), local.databricks_cluster_policy_file_variables)
+    key => jsondecode(templatestring(jsonencode(value), local.databricks_cluster_policy_file_variables))
   }
 }
