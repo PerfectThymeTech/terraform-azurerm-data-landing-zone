3933a36db1ea7ff7ec3a00d12bb0cdc2de1defea
Marvin Buss <marvin.buss@gmail.com>
2024-12-13 11:16:41 +0100

    Update budget config

--- modules/dataapplication/locals.tf
+++ modules/dataapplication/locals.tf
@@ -1,6 +1,7 @@
 locals {
   # General locals
-  prefix = "${lower(var.prefix)}-${lower(var.app_name)}-${var.environment}"
+  prefix                           = "${lower(var.prefix)}-${lower(var.app_name)}-${var.environment}"
+  budget_start_date_rotation_years = 9
 
   # Databricks locals
   databricks_enterprise_application_id = "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
