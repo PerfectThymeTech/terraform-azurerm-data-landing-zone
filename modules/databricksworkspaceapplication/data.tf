94dc00b65b0498e886327cc09310bf8a2c336f8d
Marvin Buss <marvin.buss@gmail.com>
2025-01-07 20:15:22 +0100

    lint

--- modules/databricksworkspaceapplication/data.tf
+++ modules/databricksworkspaceapplication/data.tf
@@ -3,19 +3,19 @@ data "azuread_service_principal" "service_principal" {
 }
 
 data "databricks_group" "group_admin" {
-  provider     = databricks.account
+  provider = databricks.account
 
   display_name = var.admin_group_name
 }
 
 data "databricks_group" "group_developer" {
-  provider     = databricks.account
+  provider = databricks.account
 
   display_name = var.developer_group_name
 }
 
 data "databricks_group" "group_reader" {
-  provider     = databricks.account
+  provider = databricks.account
 
   display_name = var.reader_group_name
 }
