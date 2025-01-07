3eed00bbecaf0903f2c7a98ba63fd520200945f1
Marvin Buss <marvin.buss@gmail.com>
2025-01-07 14:24:13 +0100

    Add data references

---
+++ modules/databricksworkspaceapplication/data.tf
@@ -0,0 +1,21 @@
+data "azuread_service_principal" "service_principal" {
+  display_name = var.service_principal_name
+}
+
+data "databricks_group" "group_admin" {
+  provider     = databricks.account
+
+  display_name = var.admin_group_name
+}
+
+data "databricks_group" "group_developer" {
+  provider     = databricks.account
+
+  display_name = var.developer_group_name
+}
+
+data "databricks_group" "group_reader" {
+  provider     = databricks.account
+
+  display_name = var.reader_group_name
+}
