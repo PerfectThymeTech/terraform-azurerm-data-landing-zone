05bf9106a3f0213da66fc8798d37d0697f14bc0d
Marvin Buss <marvin.buss@gmail.com>
2025-01-27 21:30:24 +0100

    Add role assignment for access connector and service credential

--- modules/dataapplication/roleassignments_msi_databricksaccessconnector.tf
+++ modules/dataapplication/roleassignments_msi_databricksaccessconnector.tf
@@ -1,68 +1,130 @@
+# Resource group role assignments
+resource "azurerm_role_assignment" "role_assignment_resource_group_app_reader_accessconnector" {
+  description          = "Role assignment to app resource group."
+  scope                = azurerm_resource_group.resource_group_app.id
+  role_definition_name = "Reader"
+  principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "role_assignment_resource_group_app_monitoring_reader_accessconnector" {
+  description          = "Role assignment to app resource group."
+  scope                = azurerm_resource_group.resource_group_app_monitoring.id
+  role_definition_name = "Reader"
+  principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "role_assignment_resource_group_storage_reader_accessconnector" {
+  description          = "Role assignment to storage resource group."
+  scope                = "${data.azurerm_subscription.current.id}/resourceGroups/${split("/", var.storage_account_ids.external)[4]}"
+  role_definition_name = "Reader"
+  principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
+  principal_type       = "ServicePrincipal"
+}
+
+# Key vault role assignments
+resource "azurerm_role_assignment" "role_assignment_key_vault_secrets_officer_accessconnector" {
+  description          = "Role assignment to key vault to create secrets."
+  scope                = module.key_vault.key_vault_id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
+  principal_type       = "ServicePrincipal"
+}
+
+# Databricks role assignments
+resource "azurerm_role_assignment" "role_assignment_databricks_workspace_reader_accessconnector" {
+  description          = "Role assignment to databricks workspace."
+  scope                = var.databricks_workspace_details["engineering"].id
+  role_definition_name = "Reader"
+  principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
+  principal_type       = "ServicePrincipal"
+}
+
+# AI service role assignments
+resource "azurerm_role_assignment" "role_assignment_ai_service_accessconnector" {
+  for_each = var.ai_services
+
+  description          = "Role assignment to the ai services."
+  scope                = module.ai_service[each.key].cognitive_account_id
+  role_definition_name = local.ai_service_kind_role_map_write[each.value.kind]
+  principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "role_assignment_cognitive_services_usages_reader_accessconnector" {
+  description          = "Cognitive Services Usages Reader to check quota for Azure Open AI models."
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Cognitive Services Usages Reader"
+  principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
+  principal_type       = "ServicePrincipal"
+}
+
 # Storage Role Assignments
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_account_external_blob_delegator" {
+resource "azurerm_role_assignment" "role_assignment_storage_account_external_blob_delegator_accessconnector" {
   scope                = var.storage_account_ids.external
   role_definition_name = "Storage Blob Delegator"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_container_external_blob_data_contributor" {
+resource "azurerm_role_assignment" "role_assignment_storage_container_external_blob_data_contributor_accessconnector" {
   scope                = azurerm_storage_container.storage_container_external.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_account_raw_blob_delegator" {
+resource "azurerm_role_assignment" "role_assignment_storage_account_raw_blob_delegator_accessconnector" {
   scope                = var.storage_account_ids.raw
   role_definition_name = "Storage Blob Delegator"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_container_raw_blob_data_contributor" {
+resource "azurerm_role_assignment" "role_assignment_storage_container_raw_blob_data_contributor_accessconnector" {
   scope                = azurerm_storage_container.storage_container_raw.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_account_enriched_blob_delegator" {
+resource "azurerm_role_assignment" "role_assignment_storage_account_enriched_blob_delegator_accessconnector" {
   scope                = var.storage_account_ids.enriched
   role_definition_name = "Storage Blob Delegator"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_container_enriched_blob_data_contributor" {
+resource "azurerm_role_assignment" "role_assignment_storage_container_enriched_blob_data_contributor_accessconnector" {
   scope                = azurerm_storage_container.storage_container_enriched.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_account_curated_blob_delegator" {
+resource "azurerm_role_assignment" "role_assignment_storage_account_curated_blob_delegator_accessconnector" {
   scope                = var.storage_account_ids.curated
   role_definition_name = "Storage Blob Delegator"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_container_curated_blob_data_contributor" {
+resource "azurerm_role_assignment" "role_assignment_storage_container_curated_blob_data_contributor_accessconnector" {
   scope                = azurerm_storage_container.storage_container_curated.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_account_workspace_blob_delegator" {
+resource "azurerm_role_assignment" "role_assignment_storage_account_workspace_blob_delegator_accessconnector" {
   scope                = var.storage_account_ids.workspace
   role_definition_name = "Storage Blob Delegator"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "accessconnector_role_assignment_storage_container_workspace_blob_data_contributor" {
+resource "azurerm_role_assignment" "role_assignment_storage_container_workspace_blob_data_contributor_accessconnector" {
   scope                = azurerm_storage_container.storage_container_workspace.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = one(module.databricks_access_connector[*].databricks_access_connector_principal_id)
