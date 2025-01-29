d3f37022e47d10275d30d335964bf03838a919df
Marvin Buss <marvin.buss@gmail.com>
2025-01-29 12:01:14 +0100

    Add missing descriptions for role assignments

--- modules/dataapplication/roleassignments_databricksresourceprovider.tf
+++ modules/dataapplication/roleassignments_databricksresourceprovider.tf
@@ -1,5 +1,6 @@
 # Key Vault Role Assignments
 resource "azurerm_role_assignment" "databricksresourceprovider_role_assignment_key_vault_secrets_user" {
+  description          = "Role assignment to enable key vault backed secret scope in Databricks."
   scope                = module.key_vault.key_vault_id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = data.azuread_service_principal.service_principal_databricks.object_id
