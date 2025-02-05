b1271389093906e3ac0b6a80e8987d3a630953b4
Marvin Buss <marvin.buss@gmail.com>
2025-02-06 00:57:41 +0100

    Add condition

--- modules/databricksdataapplication/roleassignment_service_principal_terraform_plan.tf
+++ modules/databricksdataapplication/roleassignment_service_principal_terraform_plan.tf
@@ -1,9 +1,13 @@
 resource "databricks_permission_assignment" "permission_assignment_service_principal_terraform_plan" {
+  count = var.service_principal_name_terraform_plan == "" ? 0 : 1
+
   principal_id = one(data.databricks_service_principal.service_principal_terraform_plan[*].id)
   permissions  = ["USER"]
 }
 
 resource "databricks_secret_acl" "secret_acl_service_principal_terraform_plan" {
+  count = var.service_principal_name_terraform_plan == "" ? 0 : 1
+
   principal  = one(data.databricks_service_principal.service_principal_terraform_plan[*].application_id)
   permission = "READ"
   scope      = databricks_secret_scope.secret_scope.id
