941b85ba90e3e12be03b433d922d1170509a8db6
Marvin Buss <marvin.buss@gmail.com>
2025-03-02 20:52:51 +0100

    Fix bug

--- modules/databrickscore/roleassignment_service_principal_terraform_plan.tf
+++ modules/databrickscore/roleassignment_service_principal_terraform_plan.tf
@@ -6,6 +6,10 @@ resource "databricks_permission_assignment" "permission_assignment_service_princ
 }
 
 resource "databricks_service_principal_role" "service_principal_role_account_admin_terraform_plan" {
-  service_principal_id = data.databricks_service_principal.application.id
+  provider = databricks.account
+
+  count = var.service_principal_name_terraform_plan == "" ? 0 : 1
+
+  service_principal_id = one(data.databricks_service_principal.application[*].id)
   role                 = "account_admin"
 }
