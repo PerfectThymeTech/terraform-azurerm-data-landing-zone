a83df16ce17b30a48c07c3cfdfe1fe737c25898f
Marvin Buss <marvin.buss@gmail.com>
2025-03-03 08:38:20 +0100

    Add TF PLan SP

--- modules/databrickscore/data.tf
+++ modules/databrickscore/data.tf
@@ -3,3 +3,9 @@ data "databricks_mws_network_connectivity_config" "network_connectivity_config" 
 
   name = var.databricks_network_connectivity_config_name
 }
+
+data "azuread_service_principal" "service_principal_terraform_plan" {
+  count = var.service_principal_name_terraform_plan == "" ? 0 : 1
+
+  display_name = var.service_principal_name_terraform_plan
+}
