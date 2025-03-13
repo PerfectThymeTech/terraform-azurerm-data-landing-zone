472e24e36591406f0f56a2b39e5b18991d1bb5c9
Marvin Buss <marvin.buss@gmail.com>
2025-03-13 01:42:03 +0100

    Update fabric capacity name

--- modules/core/fabric.tf
+++ modules/core/fabric.tf
@@ -10,7 +10,7 @@ module "fabric_capacity" {
   location                     = var.location
   resource_group_name          = azurerm_resource_group.resource_group_fabric.name
   tags                         = var.tags
-  fabric_capacity_name         = "${local.prefix}-fbc001"
+  fabric_capacity_name         = replace("${local.prefix}-fbc001", "-", "")
   fabric_capacity_admin_emails = var.fabric_capacity_details.admin_emails
   fabric_capacity_sku          = var.fabric_capacity_details.sku
 }
