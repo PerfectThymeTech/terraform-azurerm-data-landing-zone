64faa15504174e6a913b367c9fc846f6ae36c6cf
Marvin Buss <marvin.buss@gmail.com>
2025-03-18 13:41:23 +0100

    Add sql endpoint permissions

--- modules/databricksdataapplication/roleassignments_common.tf
+++ modules/databricksdataapplication/roleassignments_common.tf
@@ -96,6 +96,64 @@ resource "databricks_permissions" "permissions_cluster_policy" {
   ]
 }
 
+resource "databricks_permissions" "permissions_sql_endpoint" {
+  for_each = var.databricks_sql_endpoint_details
+
+  sql_endpoint_id = databricks_sql_endpoint.sql_endpoint.id
+
+  # Admin permissions
+  access_control {
+    group_name       = data.databricks_group.group_admin.display_name
+    permission_level = "CAN_MONITOR"
+  }
+  # Developer permissions
+  dynamic "access_control" {
+    for_each = var.developer_group_name == "" ? [] : [1]
+    content {
+      group_name       = one(data.databricks_group.group_developer[*].display_name)
+      permission_level = "CAN_MONITOR"
+    }
+  }
+  # Reader permissions
+  dynamic "access_control" {
+    for_each = var.reader_group_name == "" ? [] : [1]
+    content {
+      group_name       = one(data.databricks_group.group_reader[*].display_name)
+      permission_level = "CAN_USE"
+    }
+  }
+  # Service principal permissions
+  dynamic "access_control" {
+    for_each = var.service_principal_name == "" ? [] : [1]
+    content {
+      service_principal_name = one(databricks_service_principal.service_principal[*].application_id)
+      permission_level       = "CAN_MONITOR"
+    }
+  }
+  # Service principal data factory permissions
+  dynamic "access_control" {
+    for_each = var.databricks_data_factory_details.data_factory_enabled ? [1] : [0]
+    content {
+      service_principal_name = one(databricks_service_principal.service_principal_data_factory[*].application_id)
+      permission_level       = "CAN_MONITOR"
+    }
+  }
+  # UAI permissions
+  access_control {
+    service_principal_name = databricks_service_principal.service_principal_uai.application_id
+    permission_level       = "CAN_MONITOR"
+  }
+
+  depends_on = [
+    databricks_permission_assignment.permission_assignment_admin,
+    databricks_permission_assignment.permission_assignment_developer,
+    databricks_permission_assignment.permission_assignment_reader,
+    databricks_permission_assignment.permission_assignment_service_principal,
+    databricks_permission_assignment.permission_assignment_service_principal_data_factory,
+    databricks_permission_assignment.permission_assignment_uai,
+  ]
+}
+
 # resource "databricks_permissions" "permissions_budget" { # Not supported yet by provider because of missing APIs: https://github.com/databricks/terraform-provider-databricks/issues/4180
 #   budget_id = databricks_budget.budget.id
 
