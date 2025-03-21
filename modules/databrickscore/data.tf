98167065ee5059b363e374a3b7a62af2b3200f91
Marvin Buss <marvin.buss@gmail.com>
2025-03-21 10:10:32 +0100

    Remove permissions for starter warehouse

--- modules/databrickscore/data.tf
+++ modules/databrickscore/data.tf
@@ -1,3 +1,5 @@
+data "databricks_current_user" "current" {}
+
 data "databricks_mws_network_connectivity_config" "network_connectivity_config" {
   provider = databricks.account
 
@@ -8,4 +10,8 @@ data "azuread_service_principal" "service_principal_terraform_plan" {
   count = var.service_principal_name_terraform_plan == "" ? 0 : 1
 
   display_name = var.service_principal_name_terraform_plan
+}
+
+data "databricks_sql_warehouse" "sql_endpoint_starter" {
+  name = "Starter Warehouse"
 }
