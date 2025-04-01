87519a8a346b0a659cf621b406e0df1db5fad890
Marvin Buss <marvin.buss@gmail.com>
2025-04-01 12:31:25 +0200

    Update Frabric provider

--- tests/e2e/terraform.tf
+++ tests/e2e/terraform.tf
@@ -16,7 +16,7 @@ terraform {
     }
     fabric = {
       source  = "microsoft/fabric"
-      version = "~> 1.0"
+      version = "1.0.0"
     }
     databricks = {
       source  = "databricks/databricks"
