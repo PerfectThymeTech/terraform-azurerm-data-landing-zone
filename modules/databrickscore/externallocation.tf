b715aec3e344e069aba6315b996c9b236ff9046c
Marvin Buss <marvin.buss@gmail.com>
2025-05-16 00:07:28 +0200

    Switch to id

--- modules/databrickscore/externallocation.tf
+++ modules/databrickscore/externallocation.tf
@@ -9,5 +9,5 @@ resource "databricks_external_location" "external_location_engineering_default" 
   isolation_mode  = "ISOLATION_MODE_ISOLATED"
   read_only       = false
   skip_validation = false
-  url             = "abfss://${azurerm_storage_container.storage_container_engineering_default.name}@${azurerm_storage_container.storage_container_engineering_default.storage_account_name}.dfs.core.windows.net/"
+  url             = "abfss://${azurerm_storage_container.storage_container_engineering_default.name}@${reverse(split("/", azurerm_storage_container.storage_container_engineering_default.storage_account_id))[0]}.dfs.core.windows.net/"
 }
