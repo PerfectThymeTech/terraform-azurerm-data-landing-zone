2564e89c9c76b368b6224b0e368ccd36440364cf
Marvin Buss <marvin.buss@gmail.com>
2025-05-26 15:13:18 +0200

    Update network policy

--- modules/databrickscore/networkpolicy.tf
+++ modules/databrickscore/networkpolicy.tf
@@ -3,11 +3,11 @@ resource "databricks_account_network_policy" "account_network_policy" {
 
   account_id        = var.databricks_account_id
   network_policy_id = "${local.prefix}-default"
-  egress {
+  egress = {
     restriction_mode              = "RESTRICTED_ACCESS"
     allowed_internet_destinations = var.databricks_network_policy_details.allowed_internet_destinations
     allowed_storage_destinations  = var.databricks_network_policy_details.allowed_storage_destinations
-    policy_enforcement {
+    policy_enforcement = {
       dry_run_mode_product_filter = []
       enforcement_mode            = "ENFORCED"
     }
