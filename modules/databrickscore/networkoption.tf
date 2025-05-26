85179dce4b98754bb4b565bdd785f36c2664ed04
Marvin Buss <marvin.buss@gmail.com>
2025-05-26 16:24:07 +0200

    Fix reference

--- modules/databrickscore/networkoption.tf
+++ modules/databrickscore/networkoption.tf
@@ -3,6 +3,6 @@ resource "databricks_workspace_network_option" "workspace_network_option" {
 
   for_each = var.databricks_workspace_details
 
-  network_policy_id = databricks_account_network_policy.account_network_policy.id
+  network_policy_id = databricks_account_network_policy.account_network_policy.network_policy_id
   workspace_id      = each.value.workspace_id
 }
