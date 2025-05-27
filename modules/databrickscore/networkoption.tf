e3eedba1aeda2b9d775ee1e1380d276fdedf7d84
Marvin Buss <marvin.buss@gmail.com>
2025-05-27 11:25:25 +0200

    Disable network policy for now due to provider bug

--- modules/databrickscore/networkoption.tf
+++ modules/databrickscore/networkoption.tf
@@ -1,8 +1,8 @@
-resource "databricks_workspace_network_option" "workspace_network_option" {
-  provider = databricks.account
+# resource "databricks_workspace_network_option" "workspace_network_option" {
+#   provider = databricks.account
 
-  for_each = var.databricks_workspace_details
+#   for_each = var.databricks_workspace_details
 
-  network_policy_id = databricks_account_network_policy.account_network_policy.network_policy_id
-  workspace_id      = each.value.workspace_id
-}
+#   network_policy_id = databricks_account_network_policy.account_network_policy.network_policy_id
+#   workspace_id      = each.value.workspace_id
+# }
--- modules/databrickscore/networkpolicy.tf
+++ modules/databrickscore/networkpolicy.tf
@@ -1,17 +1,17 @@
-resource "databricks_account_network_policy" "account_network_policy" {
-  provider = databricks.account
+# resource "databricks_account_network_policy" "account_network_policy" {
+#   provider = databricks.account
 
-  account_id        = var.databricks_account_id
-  network_policy_id = "${local.prefix}-default"
-  egress = {
-    network_access = {
-      restriction_mode              = "RESTRICTED_ACCESS"
-      allowed_internet_destinations = var.databricks_network_policy_details.allowed_internet_destinations
-      allowed_storage_destinations  = var.databricks_network_policy_details.allowed_storage_destinations
-      policy_enforcement = {
-        dry_run_mode_product_filter = []
-        enforcement_mode            = "ENFORCED"
-      }
-    }
-  }
-}
+#   account_id        = var.databricks_account_id
+#   network_policy_id = "${local.prefix}-default"
+#   egress = {
+#     network_access = {
+#       restriction_mode              = "RESTRICTED_ACCESS"
+#       allowed_internet_destinations = var.databricks_network_policy_details.allowed_internet_destinations
+#       allowed_storage_destinations  = var.databricks_network_policy_details.allowed_storage_destinations
+#       policy_enforcement = {
+#         dry_run_mode_product_filter = []
+#         enforcement_mode            = "ENFORCED"
+#       }
+#     }
+#   }
+# }
