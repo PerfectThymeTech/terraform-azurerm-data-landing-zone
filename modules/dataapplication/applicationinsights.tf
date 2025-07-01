778ba535fcb2da43cb82f9c47094ec3c1f58790e
Marvin Buss <marvin.buss@gmail.com>
2025-07-01 13:34:40 +0200

    Fix module variable name

--- modules/dataapplication/applicationinsights.tf
+++ modules/dataapplication/applicationinsights.tf
@@ -7,7 +7,7 @@ module "application_insights" {
   location                                        = var.location
   resource_group_name                             = azurerm_resource_group.resource_group_app_monitoring.name
   tags                                            = local.tags
-  key_vault_name                                  = "${local.prefix}-ai001"
+  application_insights_name                       = "${local.prefix}-ai001"
   application_insights_application_type           = "web"
   application_insights_log_analytics_workspace_id = var.log_analytics_workspace_id
   diagnostics_configurations                      = []
