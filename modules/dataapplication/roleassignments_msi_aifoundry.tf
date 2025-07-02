b05537066c2ddca3083ce0711089d3004872b90d
Marvin Buss <marvin.buss@gmail.com>
2025-07-02 11:02:31 +0200

    Update principal id reference

--- modules/dataapplication/roleassignments_msi_aifoundry.tf
+++ modules/dataapplication/roleassignments_msi_aifoundry.tf
@@ -5,7 +5,7 @@ resource "azurerm_role_assignment" "role_assignment_storage_account_aifoundry_bl
   description          = "Role assignment for storage write operations."
   scope                = var.ai_foundry_account_details.storage_account.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -15,7 +15,7 @@ resource "azurerm_role_assignment" "role_assignment_storage_account_aifoundry_bl
   description          = "Role assignment for storage write operations."
   scope                = var.ai_foundry_account_details.storage_account.id
   role_definition_name = "Storage Blob Data Owner"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
   condition_version    = "2.0"
   condition            = <<-EOT
@@ -43,7 +43,7 @@ resource "azurerm_role_assignment" "role_assignment_cosmosdb_account_operator_ai
   description          = "Role assignment for cosmos write operations."
   scope                = var.ai_foundry_account_details.cosmos_db.id
   role_definition_name = "Cosmos DB Operator"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -53,7 +53,7 @@ resource "azurerm_cosmosdb_sql_role_assignment" "cosmosdb_sql_role_assignment_th
   resource_group_name = split("/", var.ai_foundry_account_details.cosmos_db.id)[4]
   account_name        = reverse(split("/", var.ai_foundry_account_details.cosmos_db.id))[0]
   role_definition_id  = one(data.azurerm_cosmosdb_sql_role_definition.cosmosdb_sql_role_definition[*].id)
-  principal_id        = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id        = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   scope               = "${var.ai_foundry_account_details.cosmos_db.id}/dbs/${local.cosmosdb_account_database_name}/colls/${local.ai_foundry_project_workspace_id}-${local.cosmosdb_account_database_container_thread_message_name}"
 
   depends_on = [
@@ -67,7 +67,7 @@ resource "azurerm_cosmosdb_sql_role_assignment" "cosmosdb_sql_role_assignment_sy
   resource_group_name = split("/", var.ai_foundry_account_details.cosmos_db.id)[4]
   account_name        = reverse(split("/", var.ai_foundry_account_details.cosmos_db.id))[0]
   role_definition_id  = one(data.azurerm_cosmosdb_sql_role_definition.cosmosdb_sql_role_definition[*].id)
-  principal_id        = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id        = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   scope               = "${var.ai_foundry_account_details.cosmos_db.id}/dbs/${local.cosmosdb_account_database_name}/colls/${local.ai_foundry_project_workspace_id}-${local.cosmosdb_account_database_container_system_thread_message_name}"
 
   depends_on = [
@@ -81,7 +81,7 @@ resource "azurerm_cosmosdb_sql_role_assignment" "cosmosdb_sql_role_assignment_ag
   resource_group_name = split("/", var.ai_foundry_account_details.cosmos_db.id)[4]
   account_name        = reverse(split("/", var.ai_foundry_account_details.cosmos_db.id))[0]
   role_definition_id  = one(data.azurerm_cosmosdb_sql_role_definition.cosmosdb_sql_role_definition[*].id)
-  principal_id        = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id        = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   scope               = "${var.ai_foundry_account_details.cosmos_db.id}/dbs/${local.cosmosdb_account_database_name}/colls/${local.ai_foundry_project_workspace_id}-${local.cosmosdb_account_database_container_agent_entity_store_name}"
 
   depends_on = [
@@ -95,7 +95,7 @@ resource "azurerm_role_assignment" "role_assignment_search_service_account_searc
   description          = "Role assignment for ai search write operations."
   scope                = var.ai_foundry_account_details.search_service.id
   role_definition_name = "Search Index Data Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -105,7 +105,7 @@ resource "azurerm_role_assignment" "role_assignment_search_service_account_searc
   description          = "Role assignment for ai search write operations."
   scope                = var.ai_foundry_account_details.search_service.id
   role_definition_name = "Search Service Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -122,7 +122,7 @@ resource "azurerm_role_assignment" "role_assignment_ai_service_ai_foundry_projec
   description          = "Role assignment to the ai services."
   scope                = module.ai_service[each.key].cognitive_account_id
   role_definition_name = local.ai_service_kind_role_map_write[each.value.kind]
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -133,7 +133,7 @@ resource "azurerm_role_assignment" "role_assignment_search_service_index_data_co
   description          = "Role assignment to create or manage objects in AI Search."
   scope                = one(module.ai_search[*].search_service_id)
   role_definition_name = "Search Index Data Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -143,7 +143,7 @@ resource "azurerm_role_assignment" "role_assignment_search_service_contributor_a
   description          = "Role assignment to load documents and run indexing jobs in AI Search."
   scope                = one(module.ai_search[*].search_service_id)
   role_definition_name = "Search Service Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -154,7 +154,7 @@ resource "azurerm_role_assignment" "role_assignment_storage_container_provider_b
   description          = "Role assignment to provider storage account container to read and write data."
   scope                = azurerm_storage_container.storage_container_provider[each.key].id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -164,7 +164,7 @@ resource "azurerm_role_assignment" "role_assignment_storage_container_raw_blob_d
   description          = "Role assignment to raw storage account container to read and write data."
   scope                = azurerm_storage_container.storage_container_raw.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -174,7 +174,7 @@ resource "azurerm_role_assignment" "role_assignment_storage_container_enriched_b
   description          = "Role assignment to enriched storage account container to read and write data."
   scope                = azurerm_storage_container.storage_container_enriched.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -184,7 +184,7 @@ resource "azurerm_role_assignment" "role_assignment_storage_container_curated_bl
   description          = "Role assignment to curated storage account container to read and write data."
   scope                = azurerm_storage_container.storage_container_curated.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
 
@@ -194,6 +194,6 @@ resource "azurerm_role_assignment" "role_assignment_storage_container_workspace_
   description          = "Role assignment to workspace storage account container to read and write data."
   scope                = azurerm_storage_container.storage_container_workspace.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = one(azapi_resource.ai_foundry_project[*].project_principal_id)
+  principal_id         = one(azapi_resource.ai_foundry_project[*].identity[0].principal_id)
   principal_type       = "ServicePrincipal"
 }
